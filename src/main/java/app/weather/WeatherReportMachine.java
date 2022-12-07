6973f5156d7a4cd3b0a5c170b5dcb397b8a9640d
Marion C. Striž <mcstriz@gmail.com>
2022-12-07 14:08:01 +0200

    implement WeatherReportMachine functionality

--- src/main/java/app/weather/WeatherReportMachine.java
+++ src/main/java/app/weather/WeatherReportMachine.java
@@ -1,16 +1,19 @@
 package app.weather;
 
 import app.domain.CityWeatherReport;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class WeatherReportMachine {
 
     public CityWeatherReport getFullCityWeatherReport(String city) {
-        // call handlers with the city name
-        // create full weather report with received objects from handler
-        return null;
+
+        return new CurrentWeatherHandler().getCityWeatherReport(city);
     }
 
-    public String getWeatherReportAsJson(CityWeatherReport report) {
-        return "";
+    public String getWeatherReportAsJson(CityWeatherReport report) throws JsonProcessingException {
+        ObjectMapper om = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+        return om.writeValueAsString(report);
     }
 }
