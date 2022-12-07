509d03fe9ec91f4ed410f6e000106ad5d1e96a5a
Julia <juruzu@ttu.ee>
2022-12-07 22:10:36 +0200

    add integrations tests for WeatherForecastHandler and create WeatherForecasthandler base to support the tests

---
+++ src/main/java/app/weather/WeatherForecastHandler.java
@@ -0,0 +1,20 @@
+package app.weather;
+
+import app.domain.WeatherForecastReport;
+import app.domain.WeatherReport;
+import app.dto.ForecastDto;
+
+public class WeatherForecastHandler {
+
+    public WeatherForecastReport getWeatherForecastReport(String city) {
+        return null;
+    }
+
+    private WeatherReport calculateOneDayReport(ForecastDto dto){
+        return null;
+    }
+
+    private String getDateInCorrectStringFormat(String date){
+        return "";
+    }
+}
---
+++ src/test/java/app/integration/WeatherForecastHandlerTests.java
@@ -0,0 +1,76 @@
+package app.integration;
+
+import app.domain.WeatherForecastReport;
+import app.domain.WeatherReport;
+import app.weather.WeatherForecastHandler;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class WeatherForecastHandlerTests {
+
+    private static WeatherForecastHandler weatherForecastHandler;
+    private static WeatherForecastReport weatherForecastReport;
+
+    private static List<WeatherReport> forecast;
+
+    @BeforeAll
+    public static void Initialize(){
+        String city = "Stockholm";
+        weatherForecastHandler = new WeatherForecastHandler();
+        weatherForecastReport = weatherForecastHandler.getWeatherForecastReport(city);
+        forecast = weatherForecastReport.getForecast();
+    }
+
+    @Test
+    public void givenCityName_WeatherForecastReportCannotBeNull() {
+        assertThat(weatherForecastReport).isNotNull();
+    }
+
+    @Test
+    public void givenCityName_WeatherForecastHasThreeDayReport() {
+        assertThat(forecast.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void givenCityName_FirstWeatherReportInForecastIsNotNull() {
+        assertThat(forecast.get(0)).isNotNull();
+    }
+
+    @Test
+    public void givenCityName_SecondWeatherReportInForecastIsNotNull() {
+        assertThat(forecast.get(1)).isNotNull();
+    }
+
+    @Test
+    public void givenCityName_ThirdWeatherReportInForecastIsNotNull() {
+        assertThat(forecast.get(2)).isNotNull();
+    }
+
+    @Test
+    public void givenNonExistentCity_GetWeatherForecastReport_ThrowsIllegalArgumentException() {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> weatherForecastHandler.getWeatherForecastReport("Deathstar"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"       ", "", " \t \n \t\t "})
+    public void givenEmptyString_GetWeatherForecastReport_ThrowsIllegalArgumentException(String input) {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> weatherForecastHandler.getWeatherForecastReport(input));
+    }
+
+    @Test
+    public void givenNull_GetWeatherForecastReport_ThrowsIllegalArgumentException() {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> weatherForecastHandler.getWeatherForecastReport(null));
+    }
+
+}
+
