577b5bdf68033413319cd9925e51babd2ff6e797
Marion C. Striž <mcstriz@gmail.com>
2022-12-07 14:08:35 +0200

    accept command line args and return json

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,24 @@
 package app;
 
+import app.domain.CityWeatherReport;
+import app.weather.WeatherReportMachine;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Weather API!");
-        // return full weather report as json from WeatherReportMachine and print
+    public static void main(String[] args) throws JsonProcessingException {
+        if (args.length == 0) {
+            System.out.println("Must enter city name");
+            return;
+        }
+        String city = args[0];
+
+        WeatherReportMachine machine = new WeatherReportMachine();
+        try {
+            CityWeatherReport weatherReport = machine.getFullCityWeatherReport(city);
+            System.out.println(machine.getWeatherReportAsJson(weatherReport));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
